f3bfd23d60533a4f7f5e3ed6f3cc57c0ec9991cf
Juan <juanmanuelynz@gmail.com>
2021-08-05 21:25:12 +0300

    fix black hole

--- scripts/script.js
+++ scripts/script.js
@@ -176,4 +176,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
         });
     };
     
+
+/////////////////////////////////////////////////////////////////////
+////////////////////  + send contact email //////////////////////////
+/////////////////////////////////////////////////////////////////////
+    
+    function sendEmail() {
+        Email.send({
+            SecureToken : "C973D7AD-F097-4B95-91F4-40ABC5567812",
+            To : 'juanmanuelynz@gmail.com',
+            From : "you@isp.com",
+            Subject : "This is the subject",
+            Body : "And this is the body"
+        }).then(
+          message => alert(message)
+        );   
+    }
+    
 });
